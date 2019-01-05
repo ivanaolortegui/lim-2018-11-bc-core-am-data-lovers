7fc4c9e51b16207ece668fa213da9c8ef671f9c3
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-01-05 00:50:41 -0500

    I added test for sortData function (order id pokemon), filterDataCandy function and I refactorized code

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,8 +2,12 @@ require('../src/data.js');
 
 const input = [
   { 'id': 1, 'num': '001', 'name': 'Bulbasaur', 'type': ['Grass', 'Poison'], 'candy_count': 25, 'spawn_chance': 0.69 },
-  { 'id': 2, 'num': '002', 'name': 'Ivysaur', 'type': ['Grass', 'Poison'], 'candy_count': 100, 'spawn_chance': 0.042 },  
+  { 'id': 2, 'num': '002', 'name': 'Ivysaur', 'type': ['Grass', 'Poison'], 'candy_count': 100, 'spawn_chance': 0.042 },
   { 'id': 16, 'num': '016', 'name': 'Pidgey', 'type': ['Normal', 'Flying'], 'candy_count': 12, 'spawn_chance': 15.98 }
+];
+
+const input2 = [
+  { 'candy_count': 12 }, 
 ];
 
 const output = [
@@ -16,15 +20,19 @@ const output2 = [
   { 'id': 16, 'num': '016', 'name': 'Pidgey', 'type': ['Normal', 'Flying'], 'candy_count': 12, 'spawn_chance': 15.98 },
   { 'id': 2, 'num': '002', 'name': 'Ivysaur', 'type': ['Grass', 'Poison'], 'candy_count': 100, 'spawn_chance': 0.042 },
   { 'id': 1, 'num': '001', 'name': 'Bulbasaur', 'type': ['Grass', 'Poison'], 'candy_count': 25, 'spawn_chance': 0.69 }
-]; 
+];
 
 const output3 = [
   { 'id': 1, 'num': '001', 'name': 'Bulbasaur', 'type': ['Grass', 'Poison'], 'candy_count': 25, 'spawn_chance': 0.69 },
-  { 'id': 2, 'num': '002', 'name': 'Ivysaur', 'type': ['Grass', 'Poison'], 'candy_count': 100, 'spawn_chance': 0.042 },
- 
+  { 'id': 2, 'num': '002', 'name': 'Ivysaur', 'type': ['Grass', 'Poison'], 'candy_count': 100, 'spawn_chance': 0.042 }
 ];
-const output4 = 
+
+const output4 =
   { 'id': 16, 'num': '016', 'name': 'Pidgey', 'type': ['Normal', 'Flying'], 'candy_count': 12, 'spawn_chance': 15.98 };
+
+const output5 = [ 
+  { 'candy_count': 12 },  
+]; 
 
 
 describe('dataPokemon', () => {
@@ -43,10 +51,16 @@ describe('window.dataPokemon.sortData', () => {
   it('debería retornar un nuevo array con los pokemones de Z-A', () => {
     expect(window.dataPokemon.sortData(input, 'name', 'desc')).toEqual(output2);
   });
+  it('debería retornar un nuevo array con los pokemones con id 1-151', () => {
+    expect(window.dataPokemon.sortData(input, 'id', 'asc')).toEqual(input);
+  });
+  it('debería retornar un nuevo array con los pokemones con id 151-1', () => {
+    expect(window.dataPokemon.sortData(input, 'id', 'desc')).toEqual(output2);
+  });
   it('debería retornar un nuevo array y no modificar el array original', () => {
     expect(window.dataPokemon.sortData(input, 'name', 'asc')).toEqual(input);
   });
-}); 
+});
 
 describe('window.dataPokemon.filterData', () => {
   it('Debería ser una función', () => {
@@ -63,5 +77,14 @@ describe('dataPokemon.computeStats', () => {
   });
   it('debería retornar un objeto con el pokemon con mayor probabilidad que aparezca', () => {
     expect(window.dataPokemon.computeStats(input)).toEqual(output4);
+  });
+});
+
+describe('dataPokemon.filterDataCandy', () => {
+  it('debería ser una función', () => {
+    expect(typeof window.dataPokemon.filterDataCandy).toBe('function');
+  });
+  it('debería retornar un array con el tipo de pokémon seleccionado', () => {
+    expect(window.dataPokemon.filterDataCandy(input2, 12)).toEqual(output5);
   });
 }); 
