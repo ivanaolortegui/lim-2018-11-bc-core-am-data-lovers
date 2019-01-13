855669a7e37af34b92fce11ce4043fa19bcc0e99
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-01-12 22:42:45 -0500

    Adding fetch to load the data

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ const btnCalculateMax = document.getElementById('btn-calculateMax');
 const selectFilterType = document.getElementById('select-filter-type');
 const selectFilterCandy = document.getElementById('select-filter-candy');
 
-const listData = (data) => {
-  let templateListOfCards = '';
-  data.forEach((pokemon) => {
-    const card = `
+fetch('https://raw.githubusercontent.com/Laboratoria/lim-2018-11-bc-core-am-data-lovers/master/src/data/pokemon/pokemon.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let dataPokemonFetch = data.pokemon;
+    containerAllFunction(dataPokemonFetch);
+  })
+  .catch((error) => {
+    alert('The connection failed or no response was received from the server: ' + error.message);
+  });
+
+function containerAllFunction(pokemonData) {
+  const listData = (data) => {
+    let templateListOfCards = '';
+    data.forEach((pokemon) => {
+      const card = `
    <div class="card-link">
     <article class="blog-card">
       <img src="${pokemon.img}" />
@@ -22,49 +35,38 @@ const listData = (data) => {
        </div>
     </article>
    </div>`;
-    templateListOfCards += card;
-  });
-  containerList.innerHTML = templateListOfCards;
-};
-listData(window.POKEMON.pokemon);
+      templateListOfCards += card;
+    });
+    containerList.innerHTML = templateListOfCards;
+  };
+  listData(pokemonData);
 
-const getOrderValue = () => {
-  const pokemonData = window.POKEMON.pokemon;
-  const valueOrder = selectOrder.value;
-  const valueSelectOrder = (valueOrder.split('-'));
-  const sortDataValue = window.dataPokemon.sortData(pokemonData, valueSelectOrder[0], valueSelectOrder[1], valueSelectOrder[2], valueSelectOrder[3]);
-  listData(sortDataValue);
-};
-selectOrder.addEventListener('change', getOrderValue);
+  const getOrderValue = () => {
+    const valueOrder = selectOrder.value;
+    const valueSelectOrder = (valueOrder.split('-'));
+    const sortDataValue = window.dataPokemon.sortData(pokemonData, valueSelectOrder[0], valueSelectOrder[1], valueSelectOrder[2], valueSelectOrder[3]);
+    listData(sortDataValue);
+  };
+  selectOrder.addEventListener('change', getOrderValue);
 
-const getFilterTypeValue = function() {
-  const pokemonData = window.POKEMON.pokemon;
-  const filterBy = 'Tipo';
-  const typePokemonValue = selectFilterType.value;
-  const filterDataTypeValue = window.dataPokemon.filterDataType(pokemonData, filterBy, typePokemonValue);
-  listData(filterDataTypeValue);
-};
-selectFilterType.addEventListener('change', getFilterTypeValue);
+  const getFilterTypeValue = () => {
+    const filterBy = 'Tipo';
+    const typePokemonValue = selectFilterType.value;
+    const filterDataTypeValue = window.dataPokemon.filterDataType(pokemonData, filterBy, typePokemonValue);
+    listData(filterDataTypeValue);
+  };
+  selectFilterType.addEventListener('change', getFilterTypeValue);
 
-const getCalculateValue = () => {
-  const pokemonData = window.POKEMON.pokemon;
-  const computeStatsValue = window.dataPokemon.computeStats(pokemonData);
-  listData([computeStatsValue]);
-};
-btnCalculateMax.addEventListener('click', getCalculateValue); 
+  const getCalculateValue = () => {
+    const computeStatsValue = window.dataPokemon.computeStats(pokemonData);
+    listData([computeStatsValue]);
+  };
+  btnCalculateMax.addEventListener('click', getCalculateValue);
 
-const getFilterCandyValue = () => {
-  const pokemonData = window.POKEMON.pokemon;
-  const candyPokemon = parseInt(selectFilterCandy.value);  
-  const filterDataCandyValue = window.dataPokemon.filterDataCandy(pokemonData, candyPokemon);
-  listData(filterDataCandyValue);
-};
-selectFilterCandy.addEventListener('change', getFilterCandyValue); 
-
-/* fetch('https://raw.githubusercontent.com/Laboratoria/lim-2018-11-bc-core-am-data-lovers/master/src/data/pokemon/pokemon.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    let dataPokemonfetch = data.pokemon
-  });  */
+  const getFilterCandyValue = () => {
+    const candyPokemon = parseInt(selectFilterCandy.value);
+    const filterDataCandyValue = window.dataPokemon.filterDataCandy(pokemonData, candyPokemon);
+    listData(filterDataCandyValue);
+  };
+  selectFilterCandy.addEventListener('change', getFilterCandyValue);
+}
